7682d2b8751a52861bd7637a7474c06aa5adb428
Yin Guo Wei <yinguowei@gmail.com>
2018-12-09 03:43:44 +0800

    update frontend

--- src/main/webapp/app/config/constants.ts
+++ src/main/webapp/app/config/constants.ts
@@ -15,9 +15,9 @@ export const messages = {
   DATA_ERROR_ALERT: 'Internal Error'
 };
 
-export const APP_DATE_FORMAT = 'DD/MM/YY HH:mm';
-export const APP_TIMESTAMP_FORMAT = 'DD/MM/YY HH:mm:ss';
-export const APP_LOCAL_DATE_FORMAT = 'DD/MM/YYYY';
+export const APP_DATE_FORMAT = 'YYYY-MM-DD HH:mm';
+export const APP_TIMESTAMP_FORMAT = 'YYYY-MM-DD HH:mm:ss';
+export const APP_LOCAL_DATE_FORMAT = 'YYYY-MM-DD';
 export const APP_LOCAL_DATETIME_FORMAT = 'YYYY-MM-DDThh:mm';
 export const APP_WHOLE_NUMBER_FORMAT = '0,0';
 export const APP_TWO_DIGITS_AFTER_POINT_NUMBER_FORMAT = '0,0.[00]';
